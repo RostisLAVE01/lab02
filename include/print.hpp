6e168a8445f27dd2d58146b2d622119e2349f749
RostisLAVE01 <moskalets_03@mail.ru>
2023-10-17 22:56:41 +0300

    added sources

---
+++ include/print.hpp
@@ -0,0 +1,6 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+void print(const string& text, ofstream& out);
+void print(const string& text, ostream& out = cout);
